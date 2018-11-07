27764287ae6495220cd58ca15776f063733911df
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-11-07 08:12:04 +0200

    created template for the API endPoint

--- js/models/model.js
+++ js/models/model.js
@@ -1,16 +1,11 @@
-const users = [
-    {userId: 1, username: "niomwungeri",firstname:"Clovis",lastname:"",email:"fabrice.niomwungeri@inyenyeri.com"},
-    {userId: 2, username: "admin",firstname:"admin",lastname:"admin",email:"admin@andela.com"},
-    {userId: 3, username: "clovis",firstname:"Clovis",lastname:"",email:"clovis.kabanda@inyeneri.com"},
-    {userId: 4, username: "caleb",firstname:"Caleb",lastname:"Didier",email:"caleb.didier@andel.com"},
-];
+
 const parcels = [
-    {parcelId: 1, userId: 1,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
-    {parcelId: 2, userId: 2,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
-    {parcelId: 3, userId: 2,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
-    {parcelId: 4, userId: 2,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
-    {parcelId: 5, userId: 4,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
-    {parcelId: 6, userId: 3,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
-    {parcelId: 7, userId: 4,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
-    {parcelId: 8, userId: 1,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3}
+    {parcelId: 1, userId: "niomwungeri",from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
+    {parcelId: 2, userId: "admin" ,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
+    {parcelId: 3, userId: "niomwungeri" ,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
+    {parcelId: 4, userId: "admin" ,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
+    {parcelId: 5, userId: "clovis" ,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
+    {parcelId: 6, userId: "caleb" ,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
+    {parcelId: 7, userId: "niomwungeri" ,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3},
+    {parcelId: 8, userId: "caleb" ,from:"Rwanda",to:"Kenya",length:45,lidth:42,height:2,height:3}
 ];
--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const parcels = [
+    {parcelId: 1, userId: "niomwungeri",from:"Rwanda",to:"Kenya",length:45,width:42,height:2, status: "In Transit"},
+    {parcelId: 2, userId: "admin" ,from:"Rwanda",to:"Uganda",length:45,width:42,height:2, status: "Pending"},
+    {parcelId: 3, userId: "niomwungeri" ,from:"Uganda",to:"Kenya",length:45,width:42,height:2,height:3, status: "Canceled"} ,
+    {parcelId: 4, userId: "admin" ,from:"Rwanda",to:"Somalia",length:45,width:42,height:2,height:3, status: "Pending"},
+    {parcelId: 5, userId: "clovis" ,from:"RDC",to:"Kenya",length:45,width:42,height:2,height:3, status: "Delivered"},
+    {parcelId: 6, userId: "caleb" ,from:"Rwanda",to:"Kenya",length:45,width:42,height:2,height:3, status: "Pick Up"},
+    {parcelId: 7, userId: "niomwungeri" ,from:"Rwanda",to:"Somalia",length:45,width:42,height:2,height:3, status: "Canceled"},
+    {parcelId: 8, userId: "caleb" ,from:"Ethiopia",to:"Kenya",length:45,width:42,height:2,height:3, status: "Pick Up"}
+];
+
 //permissions
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -17,15 +29,52 @@ app.use((req, res, next) => {
     }
     next();
 });
-  
-const courses = [
-    {id: 1, name: "course1"},
-    {id: 2, name: "course2"},
-    {id: 3, name: "course3"},
-    {id: 4, name: "course4"}
-];
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+//findAll
+app.get("/api/v1/parcels", (req, res) => {
+    res.send(parcels);
+});
+
+//findByParcelID
+app.get("/api/v1/parcels/:parcelId",(req, res) => {
+    let parcel = parcels.find(p => p.parcelId === parseInt(req.params.parcelId));
+    if(!parcel) return res.status(404).send("Parcel with this id >"+req.params.parcelId+"< was not found");
+    res.send(parcel);
+});
+//findByUserID
+app.get("/api/v1/users/:userId/parcels",(req, res) => {
+    let parcel = parcels.find(p => p.parcelId === parseInt(req.params.parcelId));
+    res.send(parcels);
+});
+
+//cancel parcel 
+app.put("/api/v1/parcels/:parcelId/cancel",(req, res) => {
+    let parcel = parcels.find(p => p.parcelId === parseInt(req.params.parcelId));
+    if(!parcel){
+        return res.status(404).send("Parcel with this id > "+req.params.parcelId+" < was not found");
+    }else if (!parcel) {
+        return res.status(200).send("Parcel Canceled already");
+    }else if (!parcel) {
+        return res.status(200).send("Oops, We can't cancel this parece, Parcel already Delivered");
+    }
+    res.send(parcels); 
+});
+//createParcel
+app.post("/api/v1/parcels",(req,res) =>{ 
+    let parcel = {
+        parcelId : parcels.length + 1,
+        userId : req.body.userId,
+        from: req.body.from,
+        to: req.body.to,
+        length: req.body.length,
+        width : req.body.width,
+        height: req.body.height,
+        status: "Pending"
+    };
+    parcels.push(parcel);
+    res.send(parcel);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
