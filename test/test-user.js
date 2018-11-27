1eac993d2d4adf389cd50b77b7907aa97cfb03d3
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-11-27 15:42:56 +0200

    [ft 162198528] log error for console

--- test/test-user.js
+++ test/test-user.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable no-undef */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
@@ -6,11 +7,9 @@ import app from '../server';
 chai.use(chaiHttp);
 let token = '';
 describe('POST /api/v1/auth/signup', () => {
-  
   it('should return 200 - Success', (done) => {
     chai.request(app).post('/api/v1/auth/login').send({ email: 'niomwungeri@gmail.com', password: '123' }).end((err, res) => {
       chai.expect(res.statusCode).to.be.equal(200);
-      // eslint-disable-next-line prefer-destructuring
       token = res.body.token;
       done();
     });
