f03d80e8b9e44910212e4722cea52055029c1262
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-11-28 11:19:04 +0200

    [ch 162260135] add userValidation function in middleware

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,10 +1,11 @@
+/* eslint-disable consistent-return */
 /* Code modified from a file obtained from https://github.com/olawalejarvis/reflection_app_server */
 import jwt from 'jsonwebtoken';
 import db from '../db';
+import Helper from '../controller/HelperController';
 
 const Auth = {
 
-  // eslint-disable-next-line consistent-return
   async verifyToken(req, res, next) {
     const token = req.headers['x-access-token'];
     if (!token) {
@@ -22,6 +23,15 @@ const Auth = {
     } catch (error) {
       return res.status(400).send(error);
     }
+  },
+  UserValidation(req, res, next) {
+    if (Helper.isValidatEmpty(req.body.email, req.body.password)) {
+      return res.status(400).send({ message: 'Email and Password are required', status: 400 });
+    }
+    if (!Helper.isValidEmail(req.body.email)) {
+      return res.status(400).send({ message: 'Please enter a valid email address', status: 400 });
+    }
+    next();
   },
 };
 
