c0bb96b287ab03d046f7183f135fa6534dfc2bb4
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-11-30 14:33:28 +0200

    [ch 162341301] add token verification on baseURL

--- src/routes/parcelRoute.js
+++ src/routes/parcelRoute.js
@@ -6,23 +6,23 @@ import Helper from '../controller/HelperController';
 const parcelRoute = express.Router();
 
 parcelRoute.route('/')
-  .get(Auth.verifyToken, Auth.sucureRoute, ParcelController.getAll)
-  .post(Helper.parcelValidor, Auth.verifyToken, ParcelController.create);
+  .get(Auth.sucureRoute, ParcelController.getAll)
+  .post(Helper.parcelValidor, ParcelController.create);
 // create Helper method for these routes
 parcelRoute.route('/:parcelId')
-  .get(Auth.verifyToken, ParcelController.getOne);
+  .get(ParcelController.getOne);
 
 parcelRoute.route('/:parcelId/cancel')
-  .put(Auth.verifyToken, ParcelController.cancel);
+  .put(ParcelController.cancel);
 
 parcelRoute.route('/:parcelId/destination')
-  .put(Auth.verifyToken, ParcelController.changeDestination);
+  .put(ParcelController.changeDestination);
 
 parcelRoute.route('/:parcelId/presentLocation')
-  .put(Helper.validatePresentLocation, Auth.verifyToken, Auth.sucureRoute,
+  .put(Helper.validatePresentLocation, Auth.sucureRoute,
     ParcelController.ChangePresentLocation);
 
 parcelRoute.route('/:parcelId/status')
-  .put(Helper.validateStatus, Auth.verifyToken, Auth.sucureRoute, ParcelController.changeStatus);
+  .put(Helper.validateStatus, Auth.sucureRoute, ParcelController.changeStatus);
 
 export default parcelRoute;
