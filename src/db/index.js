e43dbbca848b0dd70229fc82871c16d49808d596
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-11-30 08:54:31 +0200

    add en in pool

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,19 @@
 /* Code modified from a file obtained from https://github.com/olawalejarvis/reflection_app_server */
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-import dbconfig from './config';
+// import dbconfig from './config';
 
 dotenv.config();
 
-const env = process.env.NODE_ENV || 'development';
+// const env = process.env.NODE_ENV || 'development';
 
 const pool = new Pool({
-  connectionString: dbconfig[env],
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGNDATABASE,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD,
 });
-
 function query(text, params) {
   return new Promise((resolve, reject) => {
     pool.query(text, params)
