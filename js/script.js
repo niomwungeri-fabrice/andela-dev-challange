3217c6a52c8dcebcadb89bfdc85155848f072dcf
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-12-02 09:35:56 +0200

    [ft 162366172] add script for signup with endpoint

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-function validate(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if ( username == "admin" && password == "admin123"){
-        window.location = "admin.html"; // Redirecting to other page.
-    }else if(username == "" || password == ""){
-        alert("Username and Password are Required.")
-        window.location = "login.html"; // Redirecting to other page.
-    }else{
-        window.location = "viewParcel.html"; // Redirecting to other page.
-    }
-}
+/* eslint-disable no-undef */
+/* eslint-disable func-names */
+window.onload = function () {
+  const signUpBtn = document.getElementById('signup');
+  const fistName = document.getElementById('firstName');
+  const lastName = document.getElementById('lastName');
+  const email = document.getElementById('email');
+  const username = document.getElementById('username');
+  const password = document.getElementById('password');
+
+
+  if (signUpBtn) {
+    // eslint-disable-next-line no-use-before-define
+    signUpBtn.addEventListener('click', sinup);
+  }
+
+  async function sinup() {
+    const rawResponse = await fetch('https://andela-dev-challenge.herokuapp.com/api/v1/auth/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email.value,
+        firstName: fistName.value,
+        lastName: lastName.value,
+        password: password.value,
+        username: username.value,
+      }),
+    });
+    const content = await rawResponse.json();
+    document.getElementById('output').innerHTML = content.message;
+  }
+};
