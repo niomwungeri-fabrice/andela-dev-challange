2ece563296e10512e1ff2079c0ea6ded78bde29e
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-12-11 09:39:21 +0200

    view using vanilla js

--- UI/js/clientView.js
+++ UI/js/clientView.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
@@ -8,6 +9,8 @@ window.onload = async () => {
   let transit = 0;
   let delivered = 0;
   let cancelled = 0;
+  const pricePerKg = 100;
+  const parcelTable = document.getElementById('parcels');
   fetch('http://localhost:3000/api/v1/users/:userId/parcels', {
     method: 'GET',
     headers: {
@@ -16,9 +19,9 @@ window.onload = async () => {
     },
   })
     .then(res => res.json())
-    .then(parcels => $(document).ready(() => {
-      let tr;
+    .then((parcels) => {
       for (let index = 0; index < parcels.data.length; index++) {
+        const tr = document.createElement('tr');
         if (parcels.data[index].status === 'PENDING') {
           pending++;
         } else if (parcels.data[index].status === 'IN_TRANSIT') {
@@ -30,21 +33,33 @@ window.onload = async () => {
         } else {
           cancelled++;
         }
-        tr = $('<tr/>');
-        tr.append(`<td>${parcels.data[index].location}</td>`);
-        tr.append(`<td>${parcels.data[index].destination}</td>`);
-        tr.append(`<td>${parcels.data[index].present_location}</td>`);
-        tr.append(`<td>${parcels.data[index].weight}</td>`);
-        tr.append(`<td>${parcels.data[index].receiver_phone}</td>`);
-        tr.append(`<td>${parcels.data[index].status}</td>`);
-
-        $('#parcels').append(tr);
+        tr.innerHTML = `
+          <tr>
+            <td>${parcels.data[index].location}</td>
+            <td>${parcels.data[index].destination}</td>
+            <td>${parcels.data[index].present_location}</td>
+            <td>${parcels.data[index].weight}</td>
+            <td>${parcels.data[index].weight * pricePerKg}</td>
+            <td>${parcels.data[index].receiver_phone}</td>
+            <td>${parcels.data[index].status}</td>
+            <td>
+                <a href="editPersonal.html" onclick="editParcel(hdkjfd);return false;"> <i class="fas fa-edit"></i></a>
+                <i class="fas fa-info-circle"></i>
+                <i class="fas fa-times-circle"></i>
+            </td>
+          </tr>
+        `;
+        parcelTable.appendChild(tr);
       }
       document.getElementById('pending').innerHTML = pending;
       document.getElementById('arrived').innerHTML = arrived;
       document.getElementById('transit').innerHTML = transit;
       document.getElementById('delivered').innerHTML = delivered;
       document.getElementById('cancelled').innerHTML = cancelled;
-    }))
+      const cancelBtn = document.getElementById('cancel');
+      function editParcel(params) {
+        console.log(params);
+      }
+    })
     .catch(error => error.stack);
 };
