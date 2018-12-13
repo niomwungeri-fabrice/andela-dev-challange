a41a303480516f09fe18f695ee2de14956abe692
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2018-12-13 11:15:49 +0200

    [ft 162058285] change destination of course (user)

---
+++ js/editParcel.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+window.onload = () => {
+  const urlString = window.location.href;
+  const url = new URL(urlString);
+  const id = url.searchParams.get('id');
+  const uptateBtn = document.getElementById('createbtn-parcel');
+
+  (this.viewDetail = async () => {
+    const token = await localStorage.getItem('token');
+    fetch(`http://localhost:3000/api/v1/parcels/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'Application/JSON',
+        'x-access-token': token,
+      },
+    }).then((res) => {
+      res.json().then(async (results) => {
+        const { data } = results;
+        document.getElementById('location-parcel').value = data.location;
+        document.getElementById('location-parcel').disabled = true;
+        document.getElementById('present-location-parcel').value = data.present_location;
+        document.getElementById('present-location-parcel').disabled = true;
+        document.getElementById('destination-parcel').value = data.destination;
+        document.getElementById('weight-parcel').value = data.weight;
+        document.getElementById('weight-parcel').disabled = true;
+        document.getElementById('phone-parcel').value = data.receiver_phone;
+        document.getElementById('phone-parcel').disabled = true;
+      // update a parcel
+      }).catch(err => err);
+    });
+  })();
+
+  this.updateParcel = async () => {
+    const token = await localStorage.getItem('token');
+    const destination = document.getElementById('destination-parcel').value;
+    fetch(`http://localhost:3000/api/v1/parcels/${id}/destination`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        destination,
+      }),
+      headers: {
+        'Content-Type': 'Application/JSON',
+        'x-access-token': token,
+      },
+    }).then((res) => {
+      res.json()
+        .then(async (results) => {
+          const { message, status } = results;
+          if (status === 202) {
+            document.getElementById('output').innerHTML = message;
+            setTimeout(() => {
+              window.location.href = 'viewParcel.html';
+            }, 2000);
+          } else {
+            window.location.href = 'viewParcel.html';
+          }
+        }).catch(err => err);
+    });
+  };
+
+  if (uptateBtn) {
+    uptateBtn.addEventListener('click', updateParcel);
+  }
+};
