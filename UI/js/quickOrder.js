64b62e9543bada406d19c32f78fc02c7f87c528e
Fabrice NIYOMWUNGERI <niyomwungeri@Fabrices-MacBook-Pro.local>
2019-01-10 05:20:33 +0200

    [163087471 fix email issue]

--- UI/js/quickOrder.js
+++ UI/js/quickOrder.js
@@ -1,27 +1,23 @@
 /* eslint-disable no-undef */
 window.onload = () => {
-    const orderFrom = document.getElementById('orderFrom');
-    const orderTo = document.getElementById('orderTo');
-    const orderWeight = document.getElementById('orderWeight');
-    const quickQuote = document.getElementById('quickQuote');
-    const cancelOrder = document.getElementById('cancelOrder-btn');
-    const getQuickOrder = () => {
-                localStorage.setItem('orderFrom', orderFrom.value);
-                localStorage.setItem('orderTo', orderTo.value);
-                localStorage.setItem('orderWeight', orderWeight.value);
-                localStorage.setItem('QuickQuote', 'QuickQuote');
-                window.location.href = 'calculateQuickQuote.html';
-    };
-    if (quickQuote) {
-        quickQuote.addEventListener('click', getQuickOrder);
-    }
-    if (cancelOrder) {
-        // localStorage.clear();
-    }
-    const pricePerKg = 100;
-    const myparcelTable = document.getElementById('parcels');
-    const tr = document.createElement('tr');
-    tr.innerHTML = `
+  const orderFrom = document.getElementById('orderFrom');
+  const orderTo = document.getElementById('orderTo');
+  const orderWeight = document.getElementById('orderWeight');
+  const quickQuote = document.getElementById('quickQuote');
+  const getQuickOrder = () => {
+    localStorage.setItem('orderFrom', orderFrom.value);
+    localStorage.setItem('orderTo', orderTo.value);
+    localStorage.setItem('orderWeight', orderWeight.value);
+    localStorage.setItem('QuickQuote', 'QuickQuote');
+    window.location.href = 'calculateQuickQuote.html';
+  };
+  if (quickQuote) {
+    quickQuote.addEventListener('click', getQuickOrder);
+  }
+  const pricePerKg = 100;
+  const myparcelTable = document.getElementById('parcels');
+  const tr = document.createElement('tr');
+  tr.innerHTML = `
           <tr>
             <td>${localStorage.getItem('orderFrom')}</td>
             <td>${localStorage.getItem('orderTo')}</td>
@@ -29,6 +25,5 @@ window.onload = () => {
             <td>${localStorage.getItem('orderWeight') * pricePerKg}</td>
           </tr>
         `;
-        myparcelTable.appendChild(tr);
-  };
-  
+  myparcelTable.appendChild(tr);
+};
